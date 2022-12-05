950df013c2d9d32ad0d2955523eac725ee2e7d60
James O'Shea <james.oshea@workday.com>
2022-12-05 14:00:07 +0000

    Day 4 solutions

--- lib/aoc_2022/day_04.rb
+++ lib/aoc_2022/day_04.rb
@@ -5,17 +5,18 @@ module Aoc2022
   class Day04Part1
     def initialize(input_file)
       @input = File.read(input_file).each_line(chomp: true).to_a
+      @input.map! { |l| l.split(',').map { |z| z.split('-')[0].to_i.upto(z.split('-')[1].to_i).to_a } }
     end
 
     def run
-      # ---
+      @input.count { |a| (a[0] - a[1]).empty? || (a[1] - a[0]).empty? }
     end
   end
 
   #----
   class Day04Part2 < Day04Part1
     def run
-      # ---
+      @input.count { |a| (a[0] & a[1]).any? }
     end
   end
 
--- spec/aoc_2022_day_04_spec.rb
+++ spec/aoc_2022_day_04_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2022 do
-#   describe 'day_04' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day04.part_one(File.join(Aoc2022::INPUT_FILES, 'day_04_example.txt'))).to be(000)
-#       end
-#     end
+RSpec.describe Aoc2022 do
+  describe 'day_04' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day04.part_one(File.join(Aoc2022::INPUT_FILES, 'day_04_example.txt'))).to be(2)
+      end
+    end
 
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day04.part_one(File.join(Aoc2022::INPUT_FILES, 'day_04.txt'))).to be(000)
-#       end
-#     end
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day04.part_one(File.join(Aoc2022::INPUT_FILES, 'day_04.txt'))).to be(498)
+      end
+    end
 
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day04.part_two(File.join(Aoc2022::INPUT_FILES, 'day_04_example.txt'))).to be(000)
-#       end
-#     end
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day04.part_two(File.join(Aoc2022::INPUT_FILES, 'day_04_example.txt'))).to be(4)
+      end
+    end
 
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day04.part_two(File.join(Aoc2022::INPUT_FILES, 'day_04.txt'))).to be(000)
-#       end
-#     end
-#   end
-# end
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day04.part_two(File.join(Aoc2022::INPUT_FILES, 'day_04.txt'))).to be(859)
+      end
+    end
+  end
+end
