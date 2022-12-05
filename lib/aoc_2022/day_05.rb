eadae0e127da3b483736a7b78e9d6611b9395cf4
James O'Shea <james.oshea@workday.com>
2022-12-05 15:49:12 +0000

    Day 5 solutions

--- lib/aoc_2022/day_05.rb
+++ lib/aoc_2022/day_05.rb
@@ -8,14 +8,45 @@ module Aoc2022
     end
 
     def run
-      # ---
+      instructions.each { |i| i[:quantity].times { crate_map[i[:destination]].push(crate_map[i[:source]].pop) } }
+      top_crates
+    end
+
+    private
+
+    def crate_map
+      @crate_map ||= begin
+        hash = {}
+        padded_crate_lines.transpose.map(&:reverse).reject { |a| a[0].eql?(' ') }.each do |c|
+          hash[c[0]] = c[1..].reject { |e| e.eql?(' ') }
+        end
+        hash
+      end
+    end
+
+    def padded_crate_lines
+      crate_lines = @input.reject { |l| l[0].eql?('m') || l.empty? }
+      line_length = crate_lines.map(&:size).max
+      crate_lines.map { |c| c.chars + Array.new(line_length - c.size, ' ') }
+    end
+
+    def instructions
+      @input.select { |l| l[0].eql?('m') }.map do |i|
+        split_string = i.split
+        { quantity: split_string[1].to_i, source: split_string[3], destination: split_string[5] }
+      end
+    end
+
+    def top_crates
+      crate_map.values.map(&:last).join
     end
   end
 
   #----
   class Day05Part2 < Day05Part1
     def run
-      # ---
+      instructions.each { |i| crate_map[i[:destination]].concat(crate_map[i[:source]].pop(i[:quantity])) }
+      top_crates
     end
   end
 
--- spec/aoc_2022_day_05_spec.rb
+++ spec/aoc_2022_day_05_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2022 do
-#   describe 'day_05' do
-#     context 'part_one_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day05.part_one(File.join(Aoc2022::INPUT_FILES, 'day_05_example.txt'))).to be(000)
-#       end
-#     end
+RSpec.describe Aoc2022 do
+  describe 'day_05' do
+    context 'part_one_example' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day05.part_one(File.join(Aoc2022::INPUT_FILES, 'day_05_example.txt'))).to eq('CMZ')
+      end
+    end
 
-#     context 'part_one' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day05.part_one(File.join(Aoc2022::INPUT_FILES, 'day_05.txt'))).to be(000)
-#       end
-#     end
+    context 'part_one' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day05.part_one(File.join(Aoc2022::INPUT_FILES, 'day_05.txt'))).to eq('SPFMVDTZT')
+      end
+    end
 
-#     context 'part_two_example' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day05.part_two(File.join(Aoc2022::INPUT_FILES, 'day_05_example.txt'))).to be(000)
-#       end
-#     end
+    context 'part_two_example' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day05.part_two(File.join(Aoc2022::INPUT_FILES, 'day_05_example.txt'))).to eq('MCD')
+      end
+    end
 
-#     context 'part_two' do
-#       it 'should have the right answer' do
-#         expect(Aoc2022::Day05.part_two(File.join(Aoc2022::INPUT_FILES, 'day_05.txt'))).to be(000)
-#       end
-#     end
-#   end
-# end
+    context 'part_two' do
+      it 'should have the right answer' do
+        expect(Aoc2022::Day05.part_two(File.join(Aoc2022::INPUT_FILES, 'day_05.txt'))).to eq('ZFSJBPRFP')
+      end
+    end
+  end
+end
