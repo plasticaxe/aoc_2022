07e67fe864f806391333725d9945ce644cd2198f
James O'Shea <james.oshea@workday.com>
2022-12-12 15:58:49 +0000

    Day 10 solutions

--- lib/aoc_2022/day_10.rb
+++ lib/aoc_2022/day_10.rb
@@ -4,18 +4,48 @@ module Aoc2022
   #----
   class Day10Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input = File.read(input_file).each_line(chomp: true).to_a.map do |line|
+        cmd, val = line.split
+        { cmd: cmd.to_sym, val: val.to_i }
+      end
     end
 
     def run
-      # ---
+      [20, 60, 100, 140, 180, 220].map { |c| c * instruction_values[c] }.sum
+    end
+
+    private
+
+    def instruction_values
+      @instruction_values ||= begin
+        register = {}
+        register_changes.each_index { |index| register[index] = register_changes.first(index + 1).sum }
+        register
+      end
+    end
+
+    def register_changes
+      register_changes = [1]
+      @input.each { |i| register_changes += i[:cmd].eql?(:noop) ? [0] : [0, i[:val]] }
+      register_changes
     end
   end
 
   #----
   class Day10Part2 < Day10Part1
     def run
-      # ---
+      pixels = 0.upto(240).map { |c| sprite_position(c).include?(c % 40) ? '#' : ' ' }
+      # Print soltution
+      puts ''
+      pixels.each_slice(40) { |s| puts s.join }
+      # Create something to verify in rspec tests
+      pixels.join.bytes.sum
+    end
+
+    private
+
+    def sprite_position(cycle)
+      [instruction_values[cycle] - 1, instruction_values[cycle], instruction_values[cycle] + 1]
     end
   end
 
--- spec/aoc2022_day_10_spec.rb
+++ spec/aoc2022_day_10_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2022 do
-#   describe 'day_10' do
-#     context 'when executing part one example' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day10.part_one(File.join(Aoc2022::INPUT_FILES, 'day_10_example.txt'))).to be(000)
-#       end
-#     end
+RSpec.describe Aoc2022 do
+  describe 'day_10' do
+    context 'when executing part one example' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day10.part_one(File.join(Aoc2022::INPUT_FILES, 'day_10_example.txt'))).to be(13_360)
+      end
+    end
 
-#     context 'when executing part one' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day10.part_one(File.join(Aoc2022::INPUT_FILES, 'day_10.txt'))).to be(000)
-#       end
-#     end
+    context 'when executing part one' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day10.part_one(File.join(Aoc2022::INPUT_FILES, 'day_10.txt'))).to be(14_240)
+      end
+    end
 
-#     context 'when executing part two example' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day10.part_two(File.join(Aoc2022::INPUT_FILES, 'day_10_example.txt'))).to be(000)
-#       end
-#     end
+    context 'when executing part two example' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day10.part_two(File.join(Aoc2022::INPUT_FILES, 'day_10_example.txt'))).to be(8084)
+      end
+    end
 
-#     context 'when executing part two' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day10.part_two(File.join(Aoc2022::INPUT_FILES, 'day_10.txt'))).to be(000)
-#       end
-#     end
-#   end
-# end
+    context 'when executing part two' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day10.part_two(File.join(Aoc2022::INPUT_FILES, 'day_10.txt'))).to be(7997)
+      end
+    end
+  end
+end
