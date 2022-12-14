b04d7d931ba6072b685989bff3d1e83ea3eb3d9e
James O'Shea <james.oshea@workday.com>
2022-12-14 12:15:37 +0000

    Day 11 solutions

--- lib/aoc_2022/day_11.rb
+++ lib/aoc_2022/day_11.rb
@@ -1,21 +1,83 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 module Aoc2022
   #----
   class Day11Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @monkeys = YAML.safe_load(parsed_contents(File.read(input_file).strip))
+      @operation_counter = {}
+      @worry_divider = 3
     end
 
     def run
-      # ---
+      20.times { cycle }
+      monkey_business
+    end
+
+    private
+
+    def cycle
+      @monkeys.each_with_index do |monkey, monkey_number|
+        until monkey['items'].empty?
+          @operation_counter[monkey_number] = @operation_counter[monkey_number].to_i + 1
+          item_old = monkey['items'].shift
+          item_new = adjust_worry_level(item_old, monkey['op'])
+          pass_to_next_monkey(item_new, monkey)
+        end
+      end
+    end
+
+    def adjust_worry_level(old, operation)
+      case operation[1]
+      when '*'
+        op_value = operation[2].eql?('old') ? old : operation[2]
+        (old * op_value) / @worry_divider
+      when '+'
+        (old + operation[2]) / @worry_divider
+      else
+        raise Error
+      end
+    end
+
+    def pass_to_next_monkey(item, monkey)
+      @monkeys[destination(item, monkey)]['items'].push(modulo_of_test_lcm(item))
+    end
+
+    def destination(item, monkey)
+      (item % monkey['test']).zero? ? monkey['if_true'] : monkey['if_false']
+    end
+
+    def modulo_of_test_lcm(item)
+      (@modulo_of_test_lcm ||= {})[item] ||= item % test_lcm
+    end
+
+    def test_lcm
+      @test_lcm ||= @monkeys.map { |m| m['test'] }.inject(&:lcm)
+    end
+
+    def monkey_business
+      @operation_counter.values.max(2).inject(:*)
+    end
+
+    def parsed_contents(content)
+      content.gsub!(/Monkey.*/, '')
+      content.gsub!(/Starting items: (.*)/, '- items: [\1]')
+      content.gsub!(/Operation: new = (.*) (.*) (.*)/, '  op: [\1, "\2", \3]')
+      content.gsub!(/Test: divisible by/, '  test:')
+      content.gsub!(/If true: throw to monkey/, 'if_true:')
+      content.gsub!(/If false: throw to monkey/, 'if_false:')
+      content
     end
   end
 
   #----
   class Day11Part2 < Day11Part1
     def run
-      # ---
+      @worry_divider = 1
+      10_000.times { cycle }
+      monkey_business
     end
   end
 
--- spec/aoc2022_day_11_spec.rb
+++ spec/aoc2022_day_11_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2022 do
-#   describe 'day_11' do
-#     context 'when executing part one example' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day11.part_one(File.join(Aoc2022::INPUT_FILES, 'day_11_example.txt'))).to be(000)
-#       end
-#     end
+RSpec.describe Aoc2022 do
+  describe 'day_11' do
+    context 'when executing part one example' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day11.part_one(File.join(Aoc2022::INPUT_FILES, 'day_11_example.txt'))).to be(10_605)
+      end
+    end
 
-#     context 'when executing part one' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day11.part_one(File.join(Aoc2022::INPUT_FILES, 'day_11.txt'))).to be(000)
-#       end
-#     end
+    context 'when executing part one' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day11.part_one(File.join(Aoc2022::INPUT_FILES, 'day_11.txt'))).to be(70_176)
+      end
+    end
 
-#     context 'when executing part two example' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day11.part_two(File.join(Aoc2022::INPUT_FILES, 'day_11_example.txt'))).to be(000)
-#       end
-#     end
+    context 'when executing part two example' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day11.part_two(File.join(Aoc2022::INPUT_FILES, 'day_11_example.txt'))).to be(2_713_310_158)
+      end
+    end
 
-#     context 'when executing part two' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day11.part_two(File.join(Aoc2022::INPUT_FILES, 'day_11.txt'))).to be(000)
-#       end
-#     end
-#   end
-# end
+    context 'when executing part two' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day11.part_two(File.join(Aoc2022::INPUT_FILES, 'day_11.txt'))).to be(19_573_408_701)
+      end
+    end
+  end
+end
