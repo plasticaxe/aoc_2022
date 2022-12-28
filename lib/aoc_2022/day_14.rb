55cb13a6a6e378d0d51770ea16ab753f5b5f3f9d
James O'Shea <james.oshea@workday.com>
2022-12-28 20:55:40 +0000

    Day 14 solutions

--- lib/aoc_2022/day_14.rb
+++ lib/aoc_2022/day_14.rb
@@ -4,18 +4,128 @@ module Aoc2022
   #----
   class Day14Part1
     def initialize(input_file)
-      @input = File.read(input_file).each_line(chomp: true).to_a
+      @input = File.read(input_file).each_line(chomp: true).to_a.map { |l| l.split(' -> ') }
     end
 
     def run
-      # ---
+      counter = 0
+      loop do
+        sand = SandUnit.new(max_y_position)
+        break if sand.fall_into_position(grid).eql?(1)
+
+        counter += 1
+      end
+      counter
+    end
+
+    private
+
+    def grid
+      @grid ||= begin
+        this_grid = Set.new
+        @input.each do |i|
+          i.each_cons(2).each do |p|
+            x1, y1, x2, y2 = path_coordinates(p)
+            values_between(x1, x2).each { |x| values_between(y1, y2).each { |y| this_grid << { x: x, y: y } } }
+          end
+        end
+        this_grid
+      end
+    end
+
+    def path_coordinates(path)
+      path.flat_map { |v| v.split(',') }.map(&:to_i)
+    end
+
+    def values_between(value_one, value_two)
+      value_one < value_two ? value_one.upto(value_two).to_a : value_two.upto(value_one).to_a
+    end
+
+    def max_y_position
+      @max_y_position ||= grid.map { |p| p[:y] }.max
+    end
+
+    #----
+    class SandUnit
+      attr_accessor :x_position, :y_position
+
+      def initialize(max_y_position)
+        @x_position     = 500
+        @y_position     = 0
+        @max_y_position = max_y_position.freeze
+      end
+
+      def fall_into_position(grid)
+        loop do
+          unless free_to_fall?(grid)
+            return 2 if grid.include?({ x: @x_position, y: @y_position })
+
+            grid << { x: @x_position, y: @y_position }
+            return 0
+          end
+          return 1 if in_the_abyss?
+        end
+      end
+
+      def free_to_fall?(grid)
+        if !grid.include?({ x: @x_position, y: @y_position + 1 })
+          move_down
+        elsif !grid.include?({ x: @x_position - 1, y: @y_position + 1 })
+          move_down_and_left
+        elsif !grid.include?({ x: @x_position + 1, y: @y_position + 1 })
+          move_down_and_right
+        else
+          false
+        end
+      end
+
+      def move_down
+        @y_position += 1
+      end
+
+      def move_down_and_left
+        @x_position -= 1
+        @y_position += 1
+      end
+
+      def move_down_and_right
+        @x_position += 1
+        @y_position += 1
+      end
+
+      def in_the_abyss?
+        @y_position > @max_y_position
+      end
     end
   end
 
   #----
   class Day14Part2 < Day14Part1
     def run
-      # ---
+      draw_cave_floor
+      counter = 0
+      loop do
+        sand = SandUnit.new(max_y_position)
+        break if sand.fall_into_position(grid).eql?(2)
+
+        counter += 1
+      end
+      counter
+    end
+
+    private
+
+    def draw_cave_floor
+      adjusted_x_positions.each { |x| grid << { x: x, y: adjusted_y_position } }
+    end
+
+    def adjusted_y_position
+      @adjusted_y_position ||= grid.map { |p| p[:y] }.max + 2
+    end
+
+    def adjusted_x_positions
+      x_positions = grid.map { |p| p[:x] }
+      (x_positions.min - 1000).upto(x_positions.max + 1000).to_a
     end
   end
 
--- spec/aoc2022_day_14_spec.rb
+++ spec/aoc2022_day_14_spec.rb
@@ -1,29 +1,29 @@
 # frozen_string_literal: true
 
-# RSpec.describe Aoc2022 do
-#   describe 'day_14' do
-#     context 'when executing part one example' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day14.part_one(File.join(Aoc2022::INPUT_FILES, 'day_14_example.txt'))).to be(000)
-#       end
-#     end
+RSpec.describe Aoc2022 do
+  describe 'day_14' do
+    context 'when executing part one example' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day14.part_one(File.join(Aoc2022::INPUT_FILES, 'day_14_example.txt'))).to be(24)
+      end
+    end
 
-#     context 'when executing part one' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day14.part_one(File.join(Aoc2022::INPUT_FILES, 'day_14.txt'))).to be(000)
-#       end
-#     end
+    context 'when executing part one' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day14.part_one(File.join(Aoc2022::INPUT_FILES, 'day_14.txt'))).to be(755)
+      end
+    end
 
-#     context 'when executing part two example' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day14.part_two(File.join(Aoc2022::INPUT_FILES, 'day_14_example.txt'))).to be(000)
-#       end
-#     end
+    context 'when executing part two example' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day14.part_two(File.join(Aoc2022::INPUT_FILES, 'day_14_example.txt'))).to be(93)
+      end
+    end
 
-#     context 'when executing part two' do
-#       it 'has the right answer' do
-#         expect(Aoc2022::Day14.part_two(File.join(Aoc2022::INPUT_FILES, 'day_14.txt'))).to be(000)
-#       end
-#     end
-#   end
-# end
+    context 'when executing part two' do
+      it 'has the right answer' do
+        expect(Aoc2022::Day14.part_two(File.join(Aoc2022::INPUT_FILES, 'day_14.txt'))).to be(29_805)
+      end
+    end
+  end
+end
